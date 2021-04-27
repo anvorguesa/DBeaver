3253e36b038cccb3ad88c8b4ac36c5d2ba4426eb
anvorguesa <srliposa@gmail.com>
2021-04-27 16:16:21 -0500

    updates

---
+++ Controllers/ContactoController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CONTACTO_EJERCICIO.Models;
+
+namespace CONTACTO_EJERCICIO.Controllers
+{
+    public class ContactoController : Controller
+    {
+        private readonly ILogger<ContactoController> _logger;
+
+        public ContactoController(ILogger<ContactoController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Contacto objContacto)
+        {
+            ViewData["Message"]="Se registro el contacto";
+            return View("Index");
+        }
+    }
+}
---
+++ Models/Contacto.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace CONTACTO_EJERCICIO.Models{
+    public class Contacto{
+        public string FirstName{get;set;}
+        public string LastName{get;set;}
+        public string Email{get;set;}
+        public string Phone{get;set;}
+    }
+}
