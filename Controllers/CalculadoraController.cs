7485c0f22912617b79a3ade5a0cd168e9db07217
anvorguesa <srliposa@gmail.com>
2021-04-27 16:02:55 -0500

    estructura inicial

---
+++ Controllers/CalculadoraController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CONTACTO_EJERCICIO.Models;
+
+namespace CONTACTO_EJERCICIO.Controllers
+{
+    public class CalculadoraController : Controller
+    {
+        private readonly ILogger<CalculadoraController> _logger;
+
+        public CalculadoraController(ILogger<CalculadoraController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Execute(Calculadora objCalculadora)
+        {
+            Double res= 0.0;
+            String msj = "";
+
+            if("+" == objCalculadora.Operando){
+                res = objCalculadora.Operador1 + objCalculadora.Operador2;
+                msj = "El resultado es  "+ res;
+            }
+            if("/" == objCalculadora.Operando){
+                if(objCalculadora.Operador2 == 0){
+                    msj="El numero no puede ser divido entre 0";    
+                }else{
+                    res= objCalculadora.Operador1 / objCalculadora.Operador2;
+                    msj="El resultado es "+ res;
+                }
+            }
+            ViewData["Message"]="El resultado es"+res;
+            return View("Index");
+        }
+    }
+}
---
+++ Models/Calculadora.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace CONTACTO_EJERCICIO.Models{
+    public class Calculadora{
+        public Double Operador1{get;set;}
+        public Double Operador2{get;set;}
+        public string Operando{get;set;}
+    }
+}
